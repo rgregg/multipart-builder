9826adcd989aeb486459474954960ec599f9c412
rgregg <rgregg@users.noreply.github.com>
2015-04-07 15:37:25 -0700

    Initial check-in

---
+++ MultipartBuilder/MultipartRelatedBuilder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace MultipartBuilder
+{
+    public class MultipartRelatedBuilder
+    {
+
+        public string Boundary { get; set; }
+
+        public string ContentType { get { return string.Format("multipart/related; boundary=\"{0}\"", Boundary); } }
+
+        public List<MultipartContent> Parts { get; private set; }
+
+        public MultipartRelatedBuilder()
+        {
+            Boundary = "A100x";
+            Parts = new List<MultipartContent>();
+        }
+
+        public void WriteMultipartRelatedToFile(string outputFile)
+        {
+            Stream outputStream = File.OpenWrite(outputFile);
+            var encoding = new System.Text.UTF8Encoding(false);
+            StreamWriter writer = new StreamWriter(outputStream, encoding, 4096, true);
+
+            foreach (MultipartContent content in Parts)
+            {
+                writer.Write("--");
+                writer.WriteLine(Boundary);
+
+                // Headers
+                writer.Write(content.GetHeaderText());
+                writer.Flush();
+
+                // Body
+                if (content.StreamContent != null)
+                {
+                    content.StreamContent.CopyTo(outputStream);
+                }
+                else if (content.TextContent != null)
+                {
+                    writer.Write(content.TextContent);
+                }
+                writer.WriteLine();
+                writer.WriteLine();
+            }
+
+            writer.Write("--");
+            writer.Write(Boundary);
+            writer.WriteLine("--");
+
+            writer.Flush();
+            writer.Close();
+            outputStream.Close();
+        }
+
+    }
+
+
+    public class MultipartContent 
+    {
+        public string ContentId {get;set;}
+        public string ContentType {get;set;}
+
+        public Dictionary<string, string> Headers {get; private set;}
+
+        public string TextContent { get; set; }
+
+        public Stream StreamContent { get; set; }
+
+        public MultipartContent()
+        {
+            Headers = new Dictionary<string, string>();
+        }
+
+        public string GetHeaderText()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (!string.IsNullOrEmpty(ContentId))
+                sb.AppendLine("Content-ID: " + ContentId);
+            if (!string.IsNullOrEmpty(ContentType))
+                sb.AppendLine("Content-Type: " + ContentType);
+            foreach (var header in Headers)
+            {
+                sb.AppendLine(header.Key + ": " + header.Value);
+            }
+            sb.AppendLine();
+            return sb.ToString();
+        }
+    }
+}
+
---
+++ MultipartBuilder/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MultipartBuilder
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string pathToLocalFile;
+            string destinationName;
+            string renameBehavior = null;
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: multipartbuilder.exe <path_to_local_file> <destination_file_name> [rename_behavior]");
+                return;
+            }
+
+            pathToLocalFile = args[0];
+            destinationName = args[1];
+            if (args.Length > 2)
+                renameBehavior = args[2];
+
+            Dictionary<string, object> metadataObj = new Dictionary<string,object>();
+            metadataObj["name"] = destinationName;
+            metadataObj["file"] = new Object();
+            if (!string.IsNullOrEmpty(renameBehavior))
+                metadataObj["@name.conflictBehavior"] = renameBehavior;
+            metadataObj["@content.sourceUrl"] = "cid:content";
+
+            string metadata = JsonConvert.SerializeObject(metadataObj);
+
+            MultipartRelatedBuilder builder = new MultipartRelatedBuilder();
+            builder.Parts.Add(new MultipartContent { ContentId = "<metadata>", ContentType = "application/json", TextContent = metadata });
+
+            FileStream sourceFileStream = File.OpenRead(pathToLocalFile);
+            builder.Parts.Add(new MultipartContent { ContentId = "<content>", ContentType = "application/octet-stream", StreamContent = sourceFileStream });
+
+            builder.WriteMultipartRelatedToFile("multipart-related.bin");
+
+            Console.WriteLine("Exported multipart/related request to 'multipart-related.bin'.");
+            Console.WriteLine("HTTP Headers: ");
+            Console.WriteLine("Content-Type: " + builder.ContentType);
+
+        }
+    }
+}
